5a0b287f0935c18ebdcecfeeada47c0dd32344fa
durraalnuzha <durraalnuzha@gmail.com>
2026-02-04 11:38:01 +0300

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,31 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "standalone",
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'dwck4hd8b.cloudinary.com',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
